a3e00fa14eb999e015c4f75f5b5f98a3c990e618
JoGeorgiev <73439592+JoGeorgiev@users.noreply.github.com>
2023-02-14 19:16:06 +0100

    Update setpassword.model.ts
    Minot code refracturing and checking overall adjustments

--- Homework_3/Frontend/app/src/app/core/models/setpassword.model.ts
+++ Homework_3/Frontend/app/src/app/core/models/setpassword.model.ts
@@ -1,5 +1,6 @@
 import { Deserializable } from '../interfaces/deserializable.interface';
 export class Setpassword implements Deserializable {
+  
   password: string="";
   deserialize(input: any) {
     return Object.assign(this, input);
